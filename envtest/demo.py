3fe2bee08ddfea7cc974ba9c032e3137626b6432
Rainybunny <1732584@qq.com>
2024-07-06 15:18:34 +0800

    Updated reqs, license and some working progress.

--- envtest/demo.py
+++ envtest/demo.py
@@ -1,13 +1,28 @@
-#+Rainybunny+#
-
+import tensorcircuit as tc
+import tensorflow as tf
+import math
 import numpy as np
 from scipy import linalg as la
+import itertools
+import qiskit as qk
 
-print(np.array([2, 3]))
-print()
-print(np.array([[2], [3]]))
+## transform |x>|y> to |x>|y^f(x)>
+def transF(cir: tc.Circuit, is_balanced):
+    if is_balanced: cir.CNOT(0, cir._nqubits - 1)
+    return cir
 
-A = np.array([[1, 1, 0],
-              [0, 1, 0],
-              [0, 0, 1]])
-print(A)
+def solve(n, is_balanced):
+    cir = tc.Circuit(n + 1,
+    inputs = np.kron(np.array([0 for _ in range(1 << n)]), np.array([0, 1])))
+
+    for i in range(n + 1): cir.h(i)
+    cir = transF(cir, is_balanced)
+    for i in range(n): cir.h(i)
+
+    return cir.measure(*[i for i in range(n)], with_prob = True)
+
+n = int(input())
+is_balanced = bool(input())
+answer = bool(solve(n, is_balanced)[1])
+if (is_balanced == answer): print("Success.")
+else: print("Wrong.")
